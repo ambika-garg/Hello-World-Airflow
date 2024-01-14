f2c67bb4ea27b17740c2c9dd29170836ec570764
Ambika Garg <v-ambikagarg@microsoft.com>
2024-01-14 11:37:04 -0500

    Refactor

--- dags/hello-world.py
+++ dags/hello-world.py
@@ -9,14 +9,13 @@ with DAG(
     tags=["tutorial"]
 ) as dag:
     # Tasks are represented as operators
-    task1 = BashOperator(task_id="task1", bash_command="")
+    task1 = BashOperator(task_id="task1", bash_command="echo task2")
 
-    # task2 = BashOperator(task_id="task2", bash_command="echo task2")
+    task2 = BashOperator(task_id="task2", bash_command="echo task2")
 
-    # task3 = BashOperator(task_id="task3", bash_command="echo task3")
+    task3 = BashOperator(task_id="task3", bash_command="echo task3")
 
-    # task4 = BashOperator(task_id="task4", bash_command="echo task4")
+    task4 = BashOperator(task_id="task4", bash_command="echo task4")
 
     # Set dependencies between tasks
-    task1
-    # >> task2 >> task3 >> task4
+    task1 >> task2 >> task3 >> task4
